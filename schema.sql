65650764cfe01112f7287345b6ac61b9882d80db
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-05 18:45:35 -0500

    create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts int NOT NULL DEFAULT 0,
+    neutered boolean NOT NULL DEFAULT false,
+    weight float NOT NULL DEFAULT 0
 );
