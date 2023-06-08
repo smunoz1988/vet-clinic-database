2d61c28e2d0e6ca9f80982505b3c638f51389b0b
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-08 17:40:36 -0500

    insert required data to visits table

--- data.sql
+++ data.sql
@@ -80,3 +80,47 @@ INSERT INTO specializations (vet_id, species_id) VALUES (1, 1);
 INSERT INTO specializations (vet_id, species_id) VALUES (3, 2);
 INSERT INTO specializations (vet_id, species_id) VALUES (3, 1);
 INSERT INTO specializations (vet_id, species_id) VALUES (4, 2);
+
+/* Insert the following data for visits:
+    Agumon visited William Tatcher on May 24th, 2020.
+    Agumon visited Stephanie Mendez on Jul 22th, 2020
+    Gabumon visited Jack Harkness on Feb 2nd, 2021
+    Pikachu visited Maisy Smith on Jan 5th, 2020
+    Pikachu visited Maisy Smith on Mar 8th, 2020
+    Pikachu visited Maisy Smith on May 14th, 2020
+    Devimon visited Stephanie Mendez on May 4th, 2021
+    Charmander visited Jack Harkness on Feb 24th, 2021
+    Plantmon visited Maisy Smith on Dec 21st, 2019
+    Plantmon visited William Tatcher on Aug 10th, 2020
+    Plantmon visited Maisy Smith on Apr 7th, 2021
+    Squirtle visited Stephanie Mendez on Sep 29th, 2019
+    Angemon visited Jack Harkness on Oct 3rd, 2020
+    Angemon visited Jack Harkness on Nov 4th, 2020
+    Boarmon visited Maisy Smith on Jan 24th, 2019
+    Boarmon visited Maisy Smith on May 15th, 2019
+    Boarmon visited Maisy Smith on Feb 27th, 2020
+    Boarmon visited Maisy Smith on Aug 3rd, 2020
+    Blossom visited Stephanie Mendez on May 24th, 2020
+    Blossom visited William Tatcher on Jan 11th, 2021
+*/
+
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (1, 1, '2020-05-24');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (3, 1, '2020-07-22');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (4, 2, '2021-02-02');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 3, '2020-01-05');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 3, '2020-03-08');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 3, '2020-05-14');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (3, 4, '2021-05-04');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (4, 5, '2021-02-24');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 6, '2019-12-21');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (1, 6, '2020-08-10');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 6, '2021-04-07');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (3, 7, '2019-09-29');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (4, 8, '2020-10-03');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (4, 8, '2020-11-04');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 9, '2019-01-24');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 9, '2019-05-15');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 9, '2020-02-27');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (2, 9, '2020-08-03');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (3, 10, '2020-05-24');
+INSERT INTO visits (vet_id, animal_id, date_of_visit) VALUES (1, 10, '2021-01-11');
